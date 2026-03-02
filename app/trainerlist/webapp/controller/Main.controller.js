bec67b66ce73652a9b7b3bfdf7e0500183c9c1a9
Hector Ruiz <hector.ruiz@nubexx.es>
2026-03-02 11:18:25 +0000

    Add dialog

--- app/trainerlist/webapp/controller/Main.controller.js
+++ app/trainerlist/webapp/controller/Main.controller.js
@@ -4,6 +4,7 @@ sap.ui.define([
     "use strict";
 
     return BaseController.extend("com.nbx.trainerlist.controller.Main", {
+        _oAddTrainerDialog: null,
         onInit: function () {},
 
         onPressTrainer: function (oEvent) {
@@ -14,6 +15,46 @@ sap.ui.define([
             this.getRouter().navTo("RouteTeams", {
                 trainerId: sTrainerId
             });
+        },
+
+        
+        onAddTrainer: function () {
+    
+            if (this._oAddTrainerDialog) {
+                this._oAddTrainerDialog.open();
+                return;
+            }
+            
+            sap.ui.core.Fragment.load({
+                id: this.getView().getId(),
+                name: "com.nbx.trainerlist.view.fragment.AddTrainerDialog",
+                controller: this
+            }).then(function (oDialog) {
+                this._oAddTrainerDialog = oDialog;
+                this.getView().addDependent(this._oAddTrainerDialog);
+                this._oAddTrainerDialog.open();
+            }.bind(this));
+        },  
+        onSaveTrainer: function(){
+            const sViewId = this.getView().getId();
+
+            const sFirstName = sap.ui.core.Fragment.byId(sViewId, "inputFirstName").getValue();
+            const sLastName  = sap.ui.core.Fragment.byId(sViewId, "inputLastName").getValue();
+            const sEmail     = sap.ui.core.Fragment.byId(sViewId, "inputEmail").getValue();
+            const oModel = this.getView().getModel();
+            const oListBinding = oModel.bindList("/Trainers");
+
+            oListBinding.create({
+                FirstName: sFirstName,
+                LastName:  sLastName,
+                Email:     sEmail
+            });
+            sap.m.MessageToast.show("Trainer added successfully!");
+            this._oAddTrainerDialog.close();
+
         }
+        
+        
+
     });
 });
